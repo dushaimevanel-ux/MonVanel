83f55f7fbfa3e7cba3e584577d2c8772641aed62
dushaimevanel-ux <dushaimevanel@gmail.com>
2026-03-04 17:34:02 +0100

    ajout du code facto

---
+++ Factorielle.R
@@ -0,0 +1,17 @@
+factoriel <- function(s) {
+  if (s == 0) {
+    facto <- 1
+  } else {
+    facto <- 1
+    for (i in 1:s) {
+      facto <- facto * i
+    }
+  }
+  
+  print(paste("Factorielle de", s, "vaut =", facto))
+  return(facto)
+}
+
+p =factoriel(1005)
+
+
